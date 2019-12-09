669a3b6fa32f755946ab9e874e6edfc66b8840e6
LucaSappey <35467105+LucaSappey@users.noreply.github.com>
2019-12-09 02:08:03 +0100

    commit_langues
    Signed-off-by: LucaSappey <35467105+LucaSappey@users.noreply.github.com>

--- oxade_project/urls.py
+++ oxade_project/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
 
 urlpatterns += i18n_patterns(
     url(r'^', include('vitrine.urls'))
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
 
 if settings.DEBUG:
 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
